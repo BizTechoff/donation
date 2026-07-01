10e2075bd9c131f1c64ba6b23457425ae13510bd
Motidru <motidru@gmail.com>
2026-07-01 12:04:51 +0300

    wip: misc remaining changes
    BizTechoff(TM)

--- src/app/users/AuthGuard.ts
+++ src/app/users/AuthGuard.ts
@@ -1,10 +1,17 @@
-import { AuthenticatedGuard } from 'common-ui-elements'
 import { Injectable } from '@angular/core'
+import { AuthenticatedGuard } from 'common-ui-elements'
 import { Roles } from '../../shared/enum/roles'
 
 @Injectable()
 export class AdminGuard extends AuthenticatedGuard {
   override isAllowed() {
     return Roles.admin
+  }
+}
+
+@Injectable()
+export class AdminOrSecretary extends AuthenticatedGuard {
+  override isAllowed() {
+    return [Roles.admin, Roles.secretary]
   }
 }
